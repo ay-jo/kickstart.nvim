e60dafb589e516d1010a6257ed115bc2baaa77e4
ay-jo <ayron.johnson@gmail.com>
2026-02-26 09:21:04 -0500

    ADD: hidden file option for telescope

--- init.lua
+++ init.lua
@@ -433,6 +433,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      -- Searching hidden files.
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.find_files { hidden = true, depth = 1, file_ignore_patterns = { 'node_modules' } }
+      end, { desc = '[S]earch [.]dotfiles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
